25a564ec69b8833fd5133456f8a63562091f38ae
Donald Cummins <dc533@exeter.ac.uk>
2019-05-14 16:28:02 +0100

    Added analytic response functions

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -7,6 +7,14 @@ StepResponse <- function(Ad, Bd, G, n) {
   return(x[, -1])
 }
 
+StepResponseAnalytic <- function(A, kappa, G, n) {
+  ones <- rep(1, nrow(A))
+  func <- function(t) {
+    G/kappa[1] * (ones - expm::expAtv(A, ones, t)$eAtv)
+  }
+  return(sapply(1:n, func))
+}
+
 SimNoise <- function(Ad, Qd, Gamma0, n) {
   nboxes <- nrow(Ad)
   y <- matrix(0, nboxes, n)
@@ -50,6 +58,22 @@ TransientResponse <- function(Ad, Bd, alpha, n) {
     x[, i + 1] <- Ad %*% x[, i] + Bd * G[i]
   }
   return(x[, -1])
+}
+
+TransientResponseAnalytic <- function(A, kappa, G, n) {
+  func <- function(t) {
+    log(1.01)/log(4) * G/kappa[1] * (rep(t, nrow(A)) -
+      solve(A) %*% (expm::expm(A*t) - diag(nrow(A))) %*% rep(1, nrow(A)))
+  }
+  return(sapply(1:n, func))
+}
+
+ImpulseResponseAnalytic <- function(A, kappa, t) {
+  ones <- rep(1, nrow(A))
+  func <- function(t) {
+    -1/kappa[1] * A %*% expm::expAtv(A, ones, t)$eAtv
+  }
+  return(sapply(t, func))
 }
 
 
