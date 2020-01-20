829d11dbf980d158a75abb9c517ffc2d00cc96a1
Donald Cummins <dc533@exeter.ac.uk>
2020-01-20 22:11:06 +0000

    Changed some notation in simulation code

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,26 +1,26 @@
-StepResponse <- function(Ad, Bd, G, n) {
-  nboxes <- nrow(Ad)
-  x <- matrix(0, nboxes, n + 1)
+StepResponse <- function(Ad, Bd, F_4xCO2, n) {
+  k <- nrow(Ad)
+  x <- matrix(0, k, n + 1)
   for (i in 1:n) {
-    x[, i + 1] <- Ad %*% x[, i] + Bd * G
+    x[, i + 1] <- Ad %*% x[, i] + Bd * F_4xCO2
   }
   return(x[, -1])
 }
 
-StepResponseAnalytic <- function(A, kappa, G, n) {
+StepResponseAnalytic <- function(A, kappa, F_4xCO2, n) {
   ones <- rep(1, nrow(A))
   func <- function(t) {
-    G/kappa[1] * (ones - expm::expAtv(A, ones, t)$eAtv)
+    F_4xCO2/kappa[1] * (ones - expm::expAtv(A, ones, t)$eAtv)
   }
   return(sapply(1:n, func))
 }
 
 SimNoise <- function(Ad, Qd, Gamma0, n) {
-  nboxes <- nrow(Ad)
-  y <- matrix(0, nboxes, n)
-  y[, 1] <- t(chol(Gamma0)) %*% rnorm(nboxes)
+  k <- nrow(Ad)
+  y <- matrix(0, k, n)
+  y[, 1] <- t(chol(Gamma0)) %*% rnorm(k)
   for (i in 1:(n - 1)) {
-    y[, i + 1] <- Ad %*% y[, i] + t(chol(Qd)) %*% rnorm(nboxes)
+    y[, i + 1] <- Ad %*% y[, i] + t(chol(Qd)) %*% rnorm(k)
   }
   return(y)
 }
@@ -51,9 +51,9 @@ SimStepData <- function(C, kappa, sigma, G, epsilon, n) {
 }
 
 TransientResponse <- function(Ad, Bd, alpha, n) {
-  nboxes <- nrow(Ad)
+  k <- nrow(Ad)
   G <- alpha*log(1.01^(1:n))
-  x <- matrix(0, nboxes, n + 1)
+  x <- matrix(0, k, n + 1)
   for (i in 1:n) {
     x[, i + 1] <- Ad %*% x[, i] + Bd * G[i]
   }
